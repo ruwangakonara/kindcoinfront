9d4141d768421a9ed42a4d31eed7bf94be2f8f55
Chamalfernando <chamaldeshitha2001@gmail.com>
2024-06-18 14:23:49 +0530

    first commit with branching

---
+++ src/Pages/Admin/adminDashBoard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const adminDashBoard = () => {
+    return ( 
+        <div>
+            <h1>adminDashBoard</h1>
+        </div>
+     );
+}
+ 
+export default adminDashBoard;
