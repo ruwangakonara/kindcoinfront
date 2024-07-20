b768005df25ce762e86e9e8de413491a8ee6ee88
Chamalfernando <chamaldeshitha2001@gmail.com>
2024-07-20 17:02:15 +0530

    commit message for 20/07/2024

--- src/Pages/Admin/BeneficiaryListView/AdminBeneficiaryList.js
+++ src/Pages/Admin/BeneficiaryListView/AdminBeneficiaryList.js
@@ -1,8 +1,10 @@
 import "./AdminBeneficiaryList.css";
+import Header from "../../../Components/Admin/Header/Header";
 
 const AdminBeneficiaryList = () => {
     return (
         <div>
+            <Header />
             <h1>Admin Beneficiary List</h1>
         </div>
     );
--- src/Pages/Admin/DonorListView/AdminDonorList.js
+++
@@ -1,9 +0,0 @@
-import "./AdminDonorList.css";
-
-export default function AdminDonorList() {
-    return (
-        <div>
-            <h1>Admin Donor List</h1>
-        </div>
-    );
-}
