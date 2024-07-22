732f5aab1d5502f7fed164253fbb21285a057243
ruwangakonara <ruwangathandulakonara@gmail.com>
2024-07-22 21:43:21 +0530

    announcements/tickets merge5

--- src/Pages/Admin/CrewMemberDets/ViewCrewMemberTransactionList.js
+++
@@ -1,9 +0,0 @@
-import "./ViewCrewMemberTransactionList.css"
-
-export default function TransactionList() {
-    return (
-        <div className="TransactionList">
-            Transaction List
-        </div>
-    )
-}
