c09d39a513b33c9fda352133e99c5af4c0d48422
Eranda2001 <liviruweera@gmail.com>
2024-07-28 09:27:46 +0530

    Small Changes

--- src/Pages/CrewMember/Dashboard/CrewMemberDashboard.js
+++ src/Pages/CrewMember/Dashboard/CrewMemberDashboard.js
@@ -1,19 +1,22 @@
 import React from "react";
-import Sidebar from "../../../Components/CrewMember/Sidebar/Sidebar";
+import HeaderCrew from "../../../Components/CrewMember/Header/HeaderCrew";
+import SidebarCrew from "../../../Components/CrewMember/Sidebar/SidebarCrew";
 import DashboardCard from "../../../Components/CrewMember/DashboardCard/DashboardCard";
 import './CrewMemberDashboard.css';
 
 const CrewMemberDashboard = () => {
     return (
-        <div className="crew-member-dasboard">
-            <Sidebar />
-            <div className="dasboard-content">
-                <h1>Crew Member Default View</h1>
-                <div className="dashboard-grid">
-                    <DashboardCard title="Verify Recipient" description="Some Text" />
-                    <DashboardCard title="Verify Requests" description="Some Text" />
-                    <DashboardCard title="Verify Donation Proofs" description="Some Text" />
-                    <DashboardCard title="Token Transfer" description="Some Text" />
+        <div className="app-container">
+            <HeaderCrew />
+            <div className="main-container">
+                <SidebarCrew />
+                <div className="dashboard-content">
+                    <div className="dashboard-grid">
+                        <DashboardCard title="Verify Recipient" description="Some Text" />
+                        <DashboardCard title="Verify Requests" description="Some Text" />
+                        <DashboardCard title="Verify Donation Proofs" description="Some Text" />
+                        <DashboardCard title="Token Transfer" description="Some Text" />
+                    </div>
                 </div>
             </div>
         </div>
