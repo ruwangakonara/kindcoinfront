45871111f72fa60db2d42454d80c3a6800938ac8
Chamalfernando <chamaldeshitha2001@gmail.com>
2024-07-29 09:50:15 +0530

    changes on 29/07/2024

--- src/Components/Admin/Sidebar/SidebarAdmin.js
+++ src/Components/Admin/Sidebar/SidebarAdmin.js
@@ -1,48 +1,75 @@
-import classes from "./SidebarAdmin.module.css"
-import React, { useState } from 'react';
+// import classes from "./SidebarAdmin.module.css"
+import "./sidebarAdmin.css"
+// import React, { useState } from 'react';
+import {
+  MenuItem,
+  Header,
+  Icon,
+  Image,
+  Menu,
+  Sidebar,
+} from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 
-const SidebarAdmin = ({ onItemClick }) => {
-
-  const [activeItem, setActiveItem] = useState('');
-
-  const handleItemClick = (item) => {
-    setActiveItem(item)
-    if(onItemClick){
-      onItemClick(item);
-    }
-  };
+const SidebarAdmin = ({ visible }) => {
 
   const items = [
-    { name: 'Dashboard', path: '/admin/home' },
-    { name: 'Donors', path: '/admin/Donor_List/Donors' },
-    { name: 'Beneficiaries', path: '/admin/Beneficiary_List/Beneficiaries' },
-    { name: 'Statistics', path: '/admin/overall_stats' },
-    { name: 'Register', path: '/admin/register/crew_member' },
-    { name: 'Complaints', path: '/admin/view/complaints' },
-    { name: 'Settings', path: '/admin/settings' }
+    { name: 'Dashboard', path: '/admin/home', icon: 'home' },
+    { name: 'Donors', path: '/admin/Donor_List/Donors', icon: 'address book' },
+    { name: 'Beneficiaries', path: '/admin/Beneficiary_List/Beneficiaries', icon: 'address card outline' },
+    { name: 'Statistics', path: '/admin/overall_stats', icon: 'chart line' },
+    { name: 'Register', path: '/admin/register/crew_member', icon: 'registered' },
+    { name: 'Complaints', path: '/admin/view/complaints', icon: 'compose' },
+    { name: 'Settings', path: '/admin/settings', icon: 'settings' }
   ];
 
   return (
-    <div className={classes.sidebar}>
-      <nav>
-        <ul>
+    <div>
+      <Sidebar
+        as={Menu}
+        animation='overlay'
+        icon='labeled'
+        inverted
+        vertical
+        visible
+        width='thin'
+      >
+          {/* {items.map((item, index) => (
+            <MenuItem as={Link} to={item.path} key={index}>
+              <Icon name={item.icon} />
+              {item.name}
+            </MenuItem>
+          ))} */}
 
-          {items.map((item) => (
-            <li
-              key={item.name}
-              className={`${classes.litags} ${activeItem === item.name ? classes.active : ''}`}
-              onClick={() => handleItemClick(item.name)}
-            >
-              <Link to={item.path} 
-              style={{ color: activeItem === item.name ? 'black' : 'blue' }}
-              >
-                {item.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+          <MenuItem as='a' className="home">
+            <Icon name='home' />
+            Home
+          </MenuItem>
+          <MenuItem as='a'>
+            <Icon name='address book' />
+            Donors
+          </MenuItem>
+          <MenuItem as='a'>
+            <Icon name='address card outline' />
+            Beneficiaries
+          </MenuItem>
+          <MenuItem as='a'>
+            <Icon name='chart line' />
+            Statistics
+          </MenuItem>
+          <MenuItem as='a'>
+            <Icon name='registered' />
+            Register
+          </MenuItem>
+          <MenuItem as='a'>
+          <Icon name='compose' />
+          Complaints
+        </MenuItem>
+          <MenuItem as='a'>
+            <Icon name='settings' />
+            Settings
+          </MenuItem>
+      </Sidebar>
     </div>
   )
 }
