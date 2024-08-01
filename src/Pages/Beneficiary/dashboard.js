b873167ff10d1785701bb1c5aa435c7b0c70544b
Roshana-Fernando <roshanauni@gmail.com>
2024-08-01 10:14:36 +0530

    Added dashboard- in progress

---
+++ src/Pages/Beneficiary/dashboard.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Banner from '../../Components/Home/Banner/Banner'
+import About from '../../Components/Home/About/About'
+import Overview from '../../Components/Home/Overview/Overview'
+import Footer from '../../Components/Home/Footer/Footer'
+import Navbar from "../../Components/Home/NavBar/NavBar";
+export class dashboard extends Component {
+  render() {
+    return (
+      <div>
+          <Navbar/>
+
+          <Banner />
+          <Overview/>
+          <About/>
+          <Footer/>
+      </div>
+    )
+  }
+}
+
+export default dashboard
