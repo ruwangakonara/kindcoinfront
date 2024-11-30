07e216e12115ae0188f134a8e9a3bc250d2964c3
Chamalfernando <chamaldeshitha2001@gmail.com>
2024-11-30 11:26:00 +0530

    changes for stylings with sidebar

--- src/Pages/Admin/DonorListView/AdminDonorDetailsPg.js
+++ src/Pages/Admin/DonorListView/AdminDonorDetailsPg.js
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import classes from "./AdminDonorDetailsPg.module.css";
 import HeaderCmp from "../../../Components/Admin/Header/HeaderCmp";
-import SidebarAdminCmp from "../../../Components/Admin/Sidebar/SidebarAdminCmp";
+// import SidebarAdminCmp from "../../../Components/Admin/Sidebar/SidebarAdminCmp";
 import DefaultDashCmp from "../../../Components/Admin/DashboardComp/DefaultDashCmp";
 import AdminDonorCmp from "../../../Components/Admin/DonorDetails/AdminDonorCmp";
-
+import AdminSideBarCmp from "../../../Components/Admin/Sidebar/AdminSideBarCmp";
 const AdminDonorDetailsPg = () => {
   const { user_id } = useParams();
 
@@ -45,7 +45,8 @@ const AdminDonorDetailsPg = () => {
     return (
       <div className={classes.mainContainer}>
         <HeaderCmp />
-        <SidebarAdminCmp visible={true} />
+        {/* <SidebarAdminCmp visible={true} /> */}
+        <AdminSideBarCmp />
         <DefaultDashCmp>
           <div>Loading...</div>
         </DefaultDashCmp>
@@ -58,7 +59,8 @@ const AdminDonorDetailsPg = () => {
     return (
       <div className={classes.mainContainer}>
         <HeaderCmp />
-        <SidebarAdminCmp visible={true} />
+        {/* <SidebarAdminCmp visible={true} /> */}
+        <AdminSideBarCmp />
         <DefaultDashCmp>
           <div>Error: {error}</div>
         </DefaultDashCmp>
@@ -69,7 +71,8 @@ const AdminDonorDetailsPg = () => {
   return (
     <div className={classes.mainContainer}>
       <HeaderCmp />
-      <SidebarAdminCmp visible={true} />
+      {/* <SidebarAdminCmp visible={true} /> */}
+      <AdminSideBarCmp />
       <DefaultDashCmp>
         <h1 style={{ textAlign: "center" }}>Donor Details</h1>
         {/* Pass the donor details to AdminDonorCmp */}
--- src/Pages/Admin/home/AdminDashBoardPg.js
+++ src/Pages/Admin/home/AdminDashBoardPg.js
@@ -1,44 +1,57 @@
 import HeaderCmp from "../../../Components/Admin/Header/HeaderCmp";
-import SidebarAdminCmp from "../../../Components/Admin/Sidebar/SidebarAdminCmp";
 import CardCmp from "../../../Components/Admin/NavigationCard/CardCmp";
 import GridTypeDashCmp from "../../../Components/Admin/DashboardComp/GridTypeDashCmp";
 import DefaultDashCmp from "../../../Components/Admin/DashboardComp/DefaultDashCmp";
 import classes from "./AdminDashBoardPg.module.css";
+import AdminSideBarCmp from "../../../Components/Admin/Sidebar/AdminSideBarCmp";
+// import SidebarAdminCmp from "../../../Components/Admin/Sidebar/SidebarAdminCmp";
 // import Footer from "../../../Components/Admin/Footer/FooterCmp";
 
 const AdminDashBoardPg = () => {
   return (
     <>
+      <HeaderCmp />
       <div className={classes.mainContainer}>
-        <HeaderCmp />
-        <SidebarAdminCmp visible={true} />
-        <DefaultDashCmp>
-          <h1 style={{ textAlign: "center" }}>Admin Dashboard</h1>
-        </DefaultDashCmp>
-        <GridTypeDashCmp>
-          <CardCmp title="Donors" link="/admin/Donor_List/Donors" iconName="" />
-          <CardCmp
-            title="Benificiaries"
-            link="/admin/Beneficiary_List/Beneficiaries"
-            iconName=""
-          />
-          <CardCmp title="Statistics" link="/admin/overall_stats" iconName="" />
-          <CardCmp
-            title="Complaints"
-            link="/admin/view/complaints"
-            iconName=""
-          />
-          <CardCmp
-            title="Sign Up Crew Member"
-            link="/admin/register/crew_member"
-            iconName="registered outline"
-          />
-          <CardCmp
-            title="Assign Crew Member"
-            link="/admin/assign/crew_member"
-            iconName=""
-          />
-        </GridTypeDashCmp>
+        {/* Sidebar component */}
+        <AdminSideBarCmp />
+        {/* Main content */}
+        <div className={classes.content}>
+          <DefaultDashCmp>
+            <h1 style={{ textAlign: "center" }}>Admin Dashboard</h1>
+          </DefaultDashCmp>
+          <GridTypeDashCmp>
+            <CardCmp
+              title="Donors"
+              link="/admin/Donor_List/Donors"
+              iconName=""
+            />
+            <CardCmp
+              title="Benificiaries"
+              link="/admin/Beneficiary_List/Beneficiaries"
+              iconName=""
+            />
+            <CardCmp
+              title="Statistics"
+              link="/admin/overall_stats"
+              iconName=""
+            />
+            <CardCmp
+              title="Complaints"
+              link="/admin/view/complaints"
+              iconName=""
+            />
+            <CardCmp
+              title="Sign Up Crew Member"
+              link="/admin/register/crew_member"
+              iconName="registered outline"
+            />
+            <CardCmp
+              title="Assign Crew Member"
+              link="/admin/assign/crew_member"
+              iconName=""
+            />
+          </GridTypeDashCmp>
+        </div>
       </div>
       {/* <Footer /> */}
     </>
